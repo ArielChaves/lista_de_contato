5a95bede7b5ea353dfeb2d8c6561e12ca015b9c9
Augusto Cedric <cedric.pmi.rmp@gmail.com>
2024-03-20 18:22:21 -0300

    funcionalidade de busca implementado

--- src/containers/ListaDeContatos/index.tsx
+++ src/containers/ListaDeContatos/index.tsx
@@ -1,28 +1,44 @@
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import NovoCadastro from '../../components/Cadastro'
 import { RootReducer } from '../../store'
+import { alterarTermo } from '../../store/reducers/Filtro'
 import * as S from './styles'
 
 const ListaDeCadastro = () => {
   const { itens } = useSelector((state: RootReducer) => state.atividade)
+  const { termo } = useSelector((state: RootReducer) => state.filtro)
+
+  const dispatch = useDispatch()
+
+  const filtraContato = () => {
+    return itens.filter(
+      (item) => item.nomeContato.toLowerCase().search(termo.toLowerCase()) >= 0
+    )
+  }
+
   return (
     <main>
       <div>
         <p>Lista de contatos em react</p>
         <div>
-          <S.InputSearch type="text" placeholder="Localizar contato ...." />
+          <S.InputSearch
+            type="text"
+            placeholder="Localizar contato ...."
+            value={termo}
+            onChange={(evento) => dispatch(alterarTermo(evento.target.value))}
+          />
         </div>
         <div>
           <S.Formulario>
             <S.InputNome type="text" placeholder="Nome completo" />
             <S.InputEmail type="email" placeholder="E-mail" />
             <S.InputTelefone type="Tel" placeholder="(99) 9 9999-9999" />
-            <S.Button type="button">Cadastar</S.Button>
+            <S.Button type="button">Cadastrar</S.Button>
             <S.CancelButton type="button">Cancelar</S.CancelButton>
           </S.Formulario>
         </div>
         <ul>
-          {itens.map((p) => (
+          {filtraContato().map((p) => (
             <li key={p.nomeContato}>
               <NovoCadastro
                 id={p.id}
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
+import FiltroReducer from './reducers/Filtro'
 import atividadesReducer from './reducers/atividades'
 
 const store = configureStore({
   reducer: {
-    atividade: atividadesReducer
+    atividade: atividadesReducer,
+    filtro: FiltroReducer
   }
 })
 
