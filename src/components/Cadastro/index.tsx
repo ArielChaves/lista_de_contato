fe757888fd216c8da73a271a9e7ca2b64e2c7caa
Augusto Cedric <cedric.pmi.rmp@gmail.com>
2024-03-20 15:49:32 -0300

    controle botão editar implementado

--- src/components/Cadastro/index.tsx
+++ src/components/Cadastro/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 
 import NovoCadastroClass from '../../models/Tarefa'
@@ -8,14 +8,35 @@ import * as S from './style'
 type Props = NovoCadastroClass
 
 const NovoCadastro = ({
-  nomeContato,
-  emailContato,
-  phoneContato,
+  nomeContato: nomeContatoOriginal,
+  emailContato: emailContatoOriginal,
+  phoneContato: phoneContatoOriginal,
   id
 }: Props) => {
   const dispatch = useDispatch()
 
   const [estaEditando, setEstaEditando] = useState(false)
+  const [nomeContato, setnomeContato] = useState('')
+  const [emailContato, setemailContato] = useState('')
+  const [phoneContato, setphoneContato] = useState('')
+
+  useEffect(() => {
+    if (nomeContatoOriginal.length > 0) {
+      setnomeContato(nomeContatoOriginal)
+    }
+  }, [nomeContatoOriginal])
+
+  useEffect(() => {
+    if (emailContatoOriginal.length > 0) {
+      setemailContato(emailContatoOriginal)
+    }
+  }, [emailContatoOriginal])
+
+  useEffect(() => {
+    if (phoneContatoOriginal.length > 0) {
+      setphoneContato(phoneContatoOriginal)
+    }
+  }, [phoneContatoOriginal])
 
   return (
     <main>
@@ -23,24 +44,35 @@ const NovoCadastro = ({
         <S.InputNomeRegistrado
           type="text"
           placeholder="Nome completo"
+          disabled={!estaEditando}
           value={nomeContato}
+          onChange={(evento) => setnomeContato(evento.target.value)}
         />
         <S.InputEmailRegistrado
           type="email"
           placeholder="E-mail"
+          disabled={!estaEditando}
           value={emailContato}
+          onChange={(evento) => setemailContato(evento.target.value)}
         />
         <S.InputTelefoneRegistrado
           type="tel"
           placeholder="(99) 9 9999-9999"
+          disabled={!estaEditando}
           value={phoneContato}
+          onChange={(evento) => setphoneContato(evento.target.value)}
         />
         {estaEditando ? (
           <>
             <S.ButtonSalvar type="button">Salvar</S.ButtonSalvar>
             <S.CancelButton
               type="button"
-              onClick={() => setEstaEditando(false)}
+              onClick={() => {
+                setEstaEditando(false)
+                setnomeContato(nomeContatoOriginal),
+                  setemailContato(emailContatoOriginal),
+                  setphoneContato(phoneContatoOriginal)
+              }}
             >
               Cancelar
             </S.CancelButton>
