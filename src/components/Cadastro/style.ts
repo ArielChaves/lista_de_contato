ff7535a17760512b19ea4372768e0c8029891166
Augusto Cedric <cedric.pmi.rmp@gmail.com>
2024-03-20 00:08:59 -0300

    criação do arquivo de variavel para cores

--- src/components/Cadastro/style.ts
+++ src/components/Cadastro/style.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import variaveis from '../../styles/variaveis'
 
 // disposição do meu formulario em linha
 export const Formulario = styled.form`
@@ -9,8 +10,8 @@ export const Formulario = styled.form`
 `
 // estilo do meu botão principal de adicionar
 export const Button = styled.button`
-  background: #ff7402;
-  color: #ffffff;
+  background: ${variaveis.grafite};
+  color: ${variaveis.branco};
   border: none;
   cursor: pointer;
   padding: 8px;
@@ -19,41 +20,41 @@ export const Button = styled.button`
   height: 35px; /* Defina a altura fixa desejada */
 
   &:hover {
-    background: #ffa500;
+    background: ${variaveis.amarelo};
   }
 `
 // Quando o UseState for acionado no botão de Editar, ele vai trocar o nome para SALVAR e deve ter esta cor verde
 export const ButtonSalvar = styled(Button)`
-  background: #00a86b;
-  color: #ffffff;
+  background: ${variaveis.verde};
+  color: ${variaveis.branco};
 
   &:hover {
-    background: #00cf83;
+    background: ${variaveis.verdeHover};
   }
 `
-// Aqui passo por herança no styled() os atributos do meu Botão e trato apenas o Hover para que não fique com o mesmo hover verde
+// Aqui passo por função no styled() os atributos do meu Botão e trato apenas o Hover para que não fique com o mesmo hover verde
 export const CancelButton = styled(Button)`
-  background-color: #dc3545;
+  background-color: ${variaveis.vermelho};
   &:hover {
-    background: #e57373;
+    background: ${variaveis.vermelhoHover};
   }
 `
 // Aqui estou definindo um regra geral para os inputs
 export const InputNomeRegistrado = styled.input`
   padding: 8px;
   border: none;
-  border-bottom: 1px solid #ccc;
+  border-bottom: 1px solid ${variaveis.cinzaLight};
   background-color: transparent;
   line-height: 15px;
   font-family: 'Roboto Mono', monospace;
   width: 300px;
 `
-// Agora passando uma herança dentro do styled ()
-// Aqui está recebendo por herança as informações do (InputNomeRegistrado) e adiciono um espacamento e margin
+// Agora passando uma função dentro do styled ()
+// Aqui está recebendo por função as informações do (InputNomeRegistrado) e adiciono um espacamento e margin
 export const InputTelefoneRegistrado = styled(InputNomeRegistrado)`
   width: 215px;
 `
-// Aqui está recebendo por herança as informações do (InputNomeRegistrado) e adiciono um espacamento e margin
+// Aqui está recebendo por função as informações do (InputNomeRegistrado) e adiciono um espacamento e margin
 export const InputEmailRegistrado = styled(InputNomeRegistrado)`
   width: 300px;
 `
--- src/containers/ListaDeContatos/styles.ts
+++ src/containers/ListaDeContatos/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import variaveis from '../../styles/variaveis'
 
 // disposição do meu formulario em linha
 export const Formulario = styled.form`
@@ -10,8 +11,8 @@ export const Formulario = styled.form`
 `
 // estilo do meu botão principal de adicionar
 export const Button = styled.button`
-  background: #00a86b;
-  color: #ffffff;
+  background: ${variaveis.verde};
+  color: ${variaveis.branco};
   border: none;
   cursor: pointer;
   padding: 8px;
@@ -20,41 +21,41 @@ export const Button = styled.button`
   height: 35px; /* Defina a altura fixa desejada */
 
   &:hover {
-    background: #00cf83;
+    background: ${variaveis.verdeHover};
   }
 `
-// Aqui passo por herança no styled() os atributos do meu Botão e trato apenas o Hover para que não fique com o mesmo hover verde
+// Aqui passo por função no styled() os atributos do meu Botão e trato apenas o Hover para que não fique com o mesmo hover verde
 export const CancelButton = styled(Button)`
-  background-color: #dc3545;
+  background-color: ${variaveis.vermelho};
   width: 78px; /* Defina o tamanho fixo desejado */
   height: 35px; /* Defina a altura fixa desejada */
   &:hover {
-    background: #e57373;
+    background: ${variaveis.vermelhoHover};
   }
 `
 // Aqui estou definindo um regra geral para os inputs
 export const InputNome = styled.input`
   padding: 8px;
-  border: 1px solid #ccc;
+  border: 1px solid ${variaveis.cinzaLight};
   border-radius: 4px;
   line-height: 15px;
   font-family: 'Roboto Mono', monospace;
   width: 100%;
 `
-// Agora passando uma herança dentro do styled ()
-// Aqui está recebendo por herança as informações do (InputNome) e adiciono um espacamento e margin
+// Agora passando uma função dentro do styled ()
+// Aqui está recebendo por função as informações do (InputNome) e adiciono um espacamento e margin
 export const InputSearch = styled(InputNome)`
   width: 100%;
   margin-top: 8px;
-  background-color: #353b48;
-  color: #f5f6fa;
+  background-color: ${variaveis.grafite};
+  color: ${variaveis.cinza};
   margin-bottom: 10px;
 `
-// Aqui está recebendo por herança as informações do (InputNome) e adiciono um espacamento e margin
+// Aqui está recebendo por função as informações do (InputNome) e adiciono um espacamento e margin
 export const InputTelefone = styled(InputNome)`
   width: 70%;
 `
-// Aqui está recebendo por herança as informações do (InputNome) e adiciono um espacamento e margin
+// Aqui está recebendo por função as informações do (InputNome) e adiciono um espacamento e margin
 export const InputEmail = styled(InputNome)`
   width: 100%;
 `
